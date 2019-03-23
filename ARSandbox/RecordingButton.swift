9f23ca2eb7fdf98ae0972933289c9dbb7c8bfa39
y-mimura <yuuya.mimura@uis-inf.co.jp>
2019-03-23 17:59:08 +0900

    メジャー実装

---
+++ ARSandbox/RecordingButton.swift
@@ -0,0 +1,78 @@
+//
+//  RecordingButton.swift
+//  ARSandbox
+//
+//  Created by 三村裕矢 on 2019/03/23.
+//  Copyright © 2019 Yuya Mimura. All rights reserved.
+//
+
+import UIKit
+import ReplayKit
+
+class RecordingButton: UIButton {
+
+    var isRecording = false
+    let height:CGFloat = 50.0
+    let width:CGFloat = 100.0
+    let viewController: UIViewController!
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(_ viewController: UIViewController) {
+        self.viewController = viewController
+        
+        super.init(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        layer.position = CGPoint(x: width/2, y:viewController.view.frame.height - height)
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 1
+        setTitleColor(UIColor.white, for: .normal)
+        
+        addTarget(self, action: #selector(tapped), for: .touchUpInside)
+        setAppearance()
+        viewController.view.addSubview(self)
+    }
+    
+    @objc func tapped() {
+        if !isRecording {
+            isRecording = true
+            RPScreenRecorder.shared().startRecording { (error) in
+                if let e = error {
+                    print(e)
+                }
+                else {
+                    print("error")
+                }
+            }
+        }
+        else {
+            isRecording = false
+            RPScreenRecorder.shared().stopRecording { (previewViewController, error) in
+                previewViewController?.previewControllerDelegate = self
+                self.viewController.present(previewViewController!, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func setAppearance() {
+        var alpha: CGFloat = 1.0
+        var title = "REC"
+        if isRecording {
+            alpha = 0
+            title = ""
+        }
+        setTitle(title, for: .normal)
+        backgroundColor = UIColor(red: 0.7, green: 0, blue: 0, alpha: alpha)
+        layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: alpha).cgColor
+    }
+}
+
+extension RecordingButton : RPPreviewViewControllerDelegate {
+    func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+        DispatchQueue.main.async {
+            previewController.dismiss(animated: true, completion: nil)
+        }
+    }
+}
--- ARSandbox/ViewController.swift
+++ ARSandbox/ViewController.swift
@@ -10,43 +10,33 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+enum MarkerMode {
+    case white
+    case black
+    case none
+}
+
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var label: UILabel!
+    var recordingButton: RecordingButton!
+    var markerMode = MarkerMode.white
+    var startPosition: SCNVector3!
+    var isMeasuring = false
+    var timer: Timer!
+    var cylinderNode: SCNNode?
     
+    @IBOutlet weak var imageMaker: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        // Set the view's delegate
-//        sceneView.delegate = self
-//
-//        // Show statistics such as fps and timing information
-//        sceneView.showsStatistics = true
-//
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
+        sceneView.delegate = self
+        // デバッグ用の情報を表示する
+        sceneView.showsStatistics = true
         
-        // Viewのsceneプロパティに空のシーンを生成する
-        sceneView.scene = SCNScene()
-        
-        // 1辺の長さが20cmの立方体を生成する
-        // scneneViewの中のサイズは、1.0=1mとして現実の世界とリンクしている。
-        let node = SCNNode()
-        node.geometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-        // 表面（マテリアル）を生成する
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "block")
-        
-        // マテリアルをノードに適用
-        node.geometry?.materials = [material]
-        
-        // ノードの位置は、カメラを原点として左右：0m 上下：0m　奥に50cm
-        node.position = SCNVector3(0, 0, -0.5)
-        
-        sceneView.scene.rootNode.addChildNode(node)
+        // 録画ボタン
+        self.recordingButton = RecordingButton(self)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -57,6 +47,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        timer.fire()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -64,18 +57,185 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+        timer.invalidate()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    // Mesure Button TouchDown
+    // 計測開始
+    @IBAction func beginMesure(_ sender: UIButton) {
+        if let position = getCenter() {
+            for node in sceneView.scene.rootNode.childNodes {
+                node.removeFromParentNode()
+            }
+            startPosition = position
+            isMeasuring = true
+            
+            let spere = createSphereNode(position: startPosition,
+                                         color: UIColor.red)
+            sceneView.scene.rootNode.addChildNode(spere)
+        }
+    }
+    // 計測終了
+    func endMeasure() {
+        if !isMeasuring { return }
+        isMeasuring = false
+        
+        if let endPosition = getCenter() {
+            let sphereNode = createSphereNode(position: endPosition,
+                                              color: UIColor.red)
+            sceneView.scene.rootNode.addChildNode(sphereNode)
+            
+            let centerPosition = getCenter(startPosition: startPosition, endPosition: endPosition)
+            let centerSphereNode = createSphereNode(position: centerPosition, color: UIColor.orange)
+            sceneView.scene.rootNode.addChildNode(centerSphereNode)
+            
+            let lineNode = createLineNode(startPosition: startPosition, endPosition: endPosition, color: UIColor.white)
+            sceneView.scene.rootNode.addChildNode(lineNode)
+            
+            refreshCylinderNode(endPosition: endPosition)
+        }
+    }
+    
+    // Measure Button TouchUpInside
+    @IBAction func touchUpInside(_ sender: UIButton) {
+        endMeasure()
+    }
+    
+    // Measure Button TouchUpOutside
+    @IBAction func touchUpOutside(_ sender: UIButton) {
+        endMeasure()
+    }
+    
+    // Maker Button TouchUpInside
+    @IBAction func tapToggleMarker(_ sender: UIButton) {
+        switch markerMode {
+        case .white:
+            markerMode = .black
+            imageMaker.image = UIImage(named: "Landmark_Black")
+        case .black:
+            markerMode = .none
+            imageMaker.image = UIImage(named: "")
+        case .none:
+            markerMode = .white
+            imageMaker.image = UIImage(named: "Landmark_White")
+        }
+    }
+    
+    // 計測中に円柱の描画を更新する
+    @objc func update(tm: Timer) {
+        if isMeasuring {
+            if let endPosition = getCenter() {
+                let position = SCNVector3Make(endPosition.x - startPosition.x,
+                                              endPosition.y - startPosition.y,
+                                              endPosition.z - startPosition.z)
+                // startPosition から endPosition までの距離
+                // √(x^2 + y^2 + z^2)
+                let distance = sqrt((position.x * position.x) + (position.y * position.y) + (position.z * position.z))
+                label.text = String.init(format: "%.2fm", distance)
+                
+                refreshCylinderNode(endPosition: endPosition)
+            }
+        }
+    }
+    
+    // 円柱の更新
+    func refreshCylinderNode(endPosition: SCNVector3) {
+        if let node = cylinderNode {
+            node.removeFromParentNode()
+        }
+        cylinderNode = createCylinderNode(startPosition: startPosition,
+                                          endPosition: endPosition,
+                                          radius: 0.001, color: UIColor.yellow, transparency: 0.5)
+        sceneView.scene.rootNode.addChildNode(cylinderNode!)
+    }
+    
+    // MARK : 座標取得
+    
+    // 画面の中心を取得する
+    func getCenter() -> SCNVector3? {
+        let touchPoint = sceneView.center
+        // 衝突判定
+        // featurePoint = アンカー（平面）に依存せず座標を取得
+        let hitResults = sceneView.hitTest(touchPoint, types: [.featurePoint])
+        if !hitResults.isEmpty {
+            if let hitResult = hitResults.first {
+                return SCNVector3(hitResult.worldTransform.columns.3.x,
+                                  hitResult.worldTransform.columns.3.y,
+                                  hitResult.worldTransform.columns.3.z)
+            }
+        }
+        return nil
+    }
+    
+    // 2点間の中心座標を取得する
+    func getCenter(startPosition: SCNVector3, endPosition: SCNVector3) -> SCNVector3 {
+        let x = endPosition.x - startPosition.x
+        let y = endPosition.y - startPosition.y
+        let z = endPosition.z - startPosition.z
+        return SCNVector3Make(endPosition.x - x/2,
+                              endPosition.y - y/2,
+                              endPosition.z - z/2)
+    }
+    
+    // MARK : ノード作成
+    
+    // 円柱ノードの作成
+    func createCylinderNode(startPosition: SCNVector3, endPosition: SCNVector3,
+                            radius: CGFloat , color: UIColor, transparency: CGFloat) -> SCNNode {
+        let height = CGFloat(GLKVector3Distance(SCNVector3ToGLKVector3(startPosition),
+                                                SCNVector3ToGLKVector3(endPosition)))
+        let cylinderNode = SCNNode()
+        cylinderNode.eulerAngles.x = Float(Double.pi / 2)
+        
+        let cylinderGeometry = SCNCylinder(radius: radius, height: height)
+        cylinderGeometry.firstMaterial?.diffuse.contents = color
+        let cylinder = SCNNode(geometry: cylinderGeometry)
+        
+        cylinder.position.y = Float(-height/2)
+        cylinderNode.addChildNode(cylinder)
+        
         let node = SCNNode()
-     
+        let targetNode = SCNNode()
+        
+        if (startPosition.z < 0.0 && endPosition.z > 0.0) {
+            node.position = endPosition
+            targetNode.position = startPosition
+        }
+        else {
+            node.position = startPosition
+            targetNode.position = endPosition
+        }
+        
+        node.addChildNode(cylinderNode)
+        node.constraints = [SCNLookAtConstraint(target: targetNode)]
         return node
     }
-*/
+    
+    // 球体のノードの作成
+    func createSphereNode(position: SCNVector3, color: UIColor) -> SCNNode {
+        let sphere = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = color
+        sphere.materials = [material]
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = position
+        return sphereNode
+    }
+    
+    // 線のノードの作成
+    func createLineNode(startPosition: SCNVector3, endPosition: SCNVector3, color: UIColor) -> SCNNode {
+        let indices: [Int32] = [0, 1]
+        let source = SCNGeometrySource(vertices: [startPosition, endPosition])
+        let element = SCNGeometryElement(indices: indices, primitiveType: .line)
+        let line = SCNGeometry(sources: [source], elements: [element])
+        line.firstMaterial?.lightingModel = SCNMaterial.LightingModel.blinn
+        let lineNode = SCNNode(geometry: line)
+        lineNode.geometry?.firstMaterial?.diffuse.contents = color
+        return lineNode
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
